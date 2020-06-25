2ddde2fab67cd4a09046bdb3353d88b6ab83fe63
asudeOzdemir <asude.ozdmir@gmail.com>
2020-06-25 10:56:28 +0300

    V2

--- src/components/PersonelListesi.js
+++ src/components/PersonelListesi.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react';
+import {ProgressSpinner} from 'primereact/progressspinner';
 
 export class PersonelListe extends Component {
-
+    constructor(props) {
+        super(props);
+     
+        this.state = {
+          Users: [],
+          isLoading: true
+        };
+      }
+    
+    componentDidMount() {
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(Users => this.setState({ Users , isLoading : false}));
+        }, 5000);
+        
+      }
     render() {
+        const {isLoading} = this.state;
         return (
             <div className="p-grid">
                 <div className="p-col-12">
                     <div className="card">
                         <h1>Personel Listesi</h1>
-                        <p></p>
+                        {isLoading ?  <ProgressSpinner style={{position: 'absolute'},{width:'25%'}}/>   : ''}
+                        {
+                                this.state.Users.map(Users => 
+                                    <div key={Users.id}>
+                                        {Users.name} - {Users.email}
+                                    </div>
+                                    
+                                    ) 
+                        }
+                       
+                      
+                     
                     </div>
                 </div>
             </div>
